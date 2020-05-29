a01b2756a0cffb263fcdebc36fe0f8dfc3198ddb
Nil <nilarmengol@gmail.com>
2020-05-29 11:38:26 +0200

    menu change page

--- src/components/Admin/MenuSider/MenuSider.js
+++ src/components/Admin/MenuSider/MenuSider.js
@@ -1,28 +1,36 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, withRouter } from "react-router-dom";
 import { Layout, Menu, Icon } from "antd";
-import { HomeOutlined, MenuOutlined } from "@ant-design/icons";
+import { HomeOutlined, UserOutlined } from "@ant-design/icons";
 import "./MenuSider.scss";
 
-export default function MenuSider({ menuCollapsed }) {
+function MenuSider({ menuCollapsed, location }) {
   const { Sider } = Layout;
+
+  console.log(location.pathname);
   return (
     <Sider className="admin-sider" collapsed={menuCollapsed}>
-      <Menu theme="dark" mode="inline" defaultSelectedKeys={"1"}>
-        <Menu.Item key="1">
+      <Menu
+        theme="dark"
+        mode="inline"
+        defaultSelectedKeys={[location.pathname]}
+      >
+        <Menu.Item key="/admin">
           <Link to={"/admin"}>
             <HomeOutlined />
             <span className="nav-text">Home</span>
           </Link>
         </Menu.Item>
 
-        <Menu.Item key="2">
-          <Link to={"/admin/menu-web"}>
-            <MenuOutlined />
-            <span className="nav-text">Menu Web</span>
+        <Menu.Item key="/admin/users">
+          <Link to={"/admin/users"}>
+            <UserOutlined />
+            <span className="nav-text">Usuarios</span>
           </Link>
         </Menu.Item>
       </Menu>
     </Sider>
   );
 }
+
+export default withRouter(MenuSider);
