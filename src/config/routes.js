f2bb21890d7b8fdd3846fb73c13fc8bbe08e6598
Nil <nilarmengol@gmail.com>
2020-06-02 11:42:45 +0200

    Revert "Revert "menu change page""
    This reverts commit e0a373ba78876a957912704cf75d343d0ef807f2.

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,63 +1,65 @@
-import LayoutAdmin from '../layouts/LayoutAdmin'
-import LayoutBasic from '../layouts/LayoutBasic'
-
+import LayoutAdmin from "../layouts/LayoutAdmin";
+import LayoutBasic from "../layouts/LayoutBasic";
 
 //admin pages
-import AdminHome from '../pages/Admin'
-import AdminSignIn from '../pages/Admin/SignIn'
+import AdminHome from "../pages/Admin";
+import AdminSignIn from "../pages/Admin/SignIn";
+import Users from "../pages/Admin/Users";
 
 //pages
-import Home from '../pages/Home'
-import Contact from '../pages/Contact'
+import Home from "../pages/Home";
+import Contact from "../pages/Contact";
 
 //others
-import Error404 from '../pages/Error404'
+import Error404 from "../pages/Error404";
 
 const routes = [
-{
-    path:"/admin",
+  {
+    path: "/admin",
     component: LayoutAdmin,
     exact: false,
     routes: [
-        {
-            path:"/admin",
-            component: AdminHome,
-            exact: true
-        },
-        {
-            path:"/admin/login",
-            component: AdminSignIn,
-            exact: true
-                    },
-                    {
-                     
-                        component: Error404,
-                   
-                                }
+      {
+        path: "/admin",
+        component: AdminHome,
+        exact: true
+      },
+      {
+        path: "/admin/login",
+        component: AdminSignIn,
+        exact: true
+      },
+      {
+        path: "/admin/users",
+        component: Users,
+        exact: true
+      },
+      {
+        component: Error404
+      }
     ]
-},
-{
-    path:"/",
+  },
+
+  {
+    path: "/",
     component: LayoutBasic,
     exact: false,
     routes: [
-        {
-            path:"/",
-            component: Home,
-            exact: true
-        },
-        {
-            path:"/contact",
-            component: Contact,
-            exact: true
-                    },
-                    {
-                     
-                        component: Error404,
-                   
-                                }
+      {
+        path: "/",
+        component: Home,
+        exact: true
+      },
+      {
+        path: "/contact",
+        component: Contact,
+        exact: true
+      },
+      {
+        component: Error404
+      }
     ]
-}
-]
+  }
+];
 
-export default routes
+export default routes;
---
+++ src/pages/Admin/Users/Users.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+import "./Users.scss";
+
+export default function Users() {
+  return (
+    <div>
+      <h2>Div</h2>
+    </div>
+  );
+}
