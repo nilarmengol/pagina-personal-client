21ec8e00572a54388247d8129e1963afd25763d7
Nil <nilarmengol@gmail.com>
2020-06-02 14:12:04 +0200

    complete ListUser

--- src/components/Admin/Users/ListUsers/ListUsers.js
+++ src/components/Admin/Users/ListUsers/ListUsers.js
@@ -1,6 +1,12 @@
 import React, { useState } from "react";
-import { Switch, List, Button, Item } from "antd";
+import { Switch, List, Button, Item, Avatar } from "antd";
 import NoAvatar from "../../../../assets/img/png/user.png";
+import {
+  EditOutlined,
+  DeleteOutlined,
+  StopOutlined,
+  CheckOutlined,
+} from "@ant-design/icons";
 
 import "./ListUsers.scss";
 
@@ -18,15 +24,90 @@ export default function ListUsers({ usersActive, usersInactive }) {
           {viewUsersActives ? "Usuarios Activos" : "Usuarios Inactivos"}
         </span>
       </div>
-      {viewUsersActives ? <UsersActive /> : <UsersInactive />}
+      {viewUsersActives ? (
+        <UsersActive usersActive={usersActive} />
+      ) : (
+        <UsersInactive usersInactive={usersInactive} />
+      )}
     </div>
   );
 }
 
-function UsersActive() {
-  return <h3>Lista de usuarios activos</h3>;
+function UsersActive({ usersActive }) {
+  return (
+    <List
+      className="users-active"
+      itemLayout="horizontal"
+      dataSource={usersActive}
+      renderItem={(user) => (
+        <List.Item
+          actions={[
+            <Button
+              type="primary"
+              onClick={() => console.log("Editar Usuario")}
+            >
+              <EditOutlined />
+            </Button>,
+            <Button
+              type="danger"
+              onClick={() => console.log("Desactivar Usuario")}
+            >
+              <StopOutlined />
+            </Button>,
+            <Button
+              type="danger"
+              onClick={() => console.log("Desactivar Usuario")}
+            >
+              <DeleteOutlined />
+            </Button>,
+          ]}
+        >
+          <List.Item.Meta
+            avatar={<Avatar src={user.avatar ? user.avatar : NoAvatar} />}
+            title={`${user.name ? user.name : "..."}${
+              user.lastname ? user.lastname : "..."
+            }`}
+            description={user.email}
+          ></List.Item.Meta>
+        </List.Item>
+      )}
+    ></List>
+  );
 }
 
-function UsersInactive() {
-  return <h3>Lista de usuarios Inactivos</h3>;
+function UsersInactive({ usersInactive }) {
+  return (
+    <List
+      className="users-active"
+      itemLayout="horizontal"
+      dataSource={usersInactive}
+      renderItem={(user) => (
+        <List.Item
+          actions={[
+            <Button
+              type="primary"
+              onClick={() => console.log("Editar Usuario")}
+            >
+              <CheckOutlined />
+            </Button>,
+
+            <Button
+              type="danger"
+              onClick={() => console.log("Desactivar Usuario")}
+            >
+              <DeleteOutlined />
+            </Button>,
+          ]}
+        >
+          <List.Item.Meta
+            avatar={<Avatar src={user.avatar ? user.avatar : NoAvatar} />}
+            title={`${user.name ? user.name : "..."}${
+              user.lastname ? user.lastname : "..."
+            }`}
+            description={user.email}
+          ></List.Item.Meta>
+        </List.Item>
+      )}
+    ></List>
+  );
 }
