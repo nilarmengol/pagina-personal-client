6a7b2b8c6fdd5a754ba06820c99f2125511f1dc1
Nil <nilarmengol@gmail.com>
2020-06-02 12:28:58 +0200

    getUserActive

--- src/api/user.js
+++ src/api/user.js
@@ -44,3 +44,45 @@ export function signInApi(data) {
       return err.message;
     });
 }
+
+export function getUserApi(token) {
+  const url = `${basePath}/${apiVersion}/users`;
+  const params = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token
+    }
+  };
+  return fetch(url, params)
+    .then(response => {
+      return response.json();
+    })
+    .then(result => {
+      return result;
+    })
+    .catch(err => {
+      return err.message;
+    });
+}
+
+export function getUsersActiveApi(token, status) {
+  const url = `${basePath}/${apiVersion}/users-active?active=${status}`;
+  const params = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: token
+    }
+  };
+  return fetch(url, params)
+    .then(response => {
+      return response.json();
+    })
+    .then(result => {
+      return result;
+    })
+    .catch(err => {
+      return err.message;
+    });
+}
--- src/pages/Admin/Users/Users.js
+++ src/pages/Admin/Users/Users.js
@@ -1,8 +1,24 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { getAccessTokenApi } from "../../../api/auth";
+import { getUsersActiveApi } from "../../../api/user";
 
 import "./Users.scss";
 
 export default function Users() {
+  const [usersActive, setUsersActive] = useState([]);
+  const [usersInactive, setUsersInactive] = useState([]);
+  const token = getAccessTokenApi();
+  console.log("usersActive", usersActive);
+  console.log("usersInactive", usersInactive);
+  useEffect(() => {
+    getUsersActiveApi(token, true).then((response) => {
+      setUsersActive(response);
+    });
+    getUsersActiveApi(token, false).then((response) => {
+      setUsersInactive(response);
+    });
+  }, [token]);
+
   return (
     <div>
       <h2>Div</h2>
