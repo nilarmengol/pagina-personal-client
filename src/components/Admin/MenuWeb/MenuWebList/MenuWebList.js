f6fcdc8b42dd143c0074da08a42d4248dc8fb151
Nil <nilarmengol@gmail.com>
2020-06-05 12:15:58 +0200

    MenuList

--- src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
+++ src/components/Admin/MenuWeb/MenuWebList/MenuWebList.js
@@ -1,12 +1,62 @@
 import React, { useState, useEffect } from "react";
+import { Switch, List, Button, Modal as ModalAntd, notification } from "antd";
+import Modal from "../../../Modal";
+import DragSortableList from "react-drag-sortable";
+import { EditOutlined, DeleteOutlined } from "@ant-design/icons";
+
+import "./MenuWebList.scss";
 
 export default function MenuWebList({ menu, setReloadMenuWeb }) {
+  const [isVisibleModal, setIsVisibleModal] = useState(false);
+  const [listItems, setListItems] = useState([]);
+  const [modalTitle, setModalTitle] = useState("");
+  const [modalContent, setModalContent] = useState(null);
+
+  const onSort = (sortedList, dropEvent) => {
+    console.log("sortedList", sortedList, dropEvent);
+  };
+  console.log(listItems);
+
+  useEffect(() => {
+    const listItemsArray = [];
+    menu.forEach(item => {
+      listItemsArray.push({
+        content: <MenuItem item={item} />
+      });
+    });
+
+    setListItems(listItemsArray);
+  }, [menu]);
+
   return (
-    <div>
-      <h1>Menu Web</h1>
-      {menu.map(item => (
-        <p key={item._id}>{item.title}</p>
-      ))}
+    <div className="menu-web-list">
+      <div className="menu-web-list__header">
+        <Button type="primary">Menu</Button>
+      </div>
+      <div className="menu-web-list__items">
+        <DragSortableList items={listItems} onSort={onSort} type="vertical" />
+      </div>
     </div>
+  );
+}
+
+function MenuItem({ item }) {
+  return (
+    <List.Item
+      actions={[
+        <Switch defaultChecked={item.active} />,
+        <Button type="primary">
+          <EditOutlined />
+        </Button>,
+        <Button type="danger">
+          <DeleteOutlined />
+        </Button>
+      ]}
+    >
+      <List.Item.Meta
+        title={item.title}
+        description={item.url}
+      ></List.Item.Meta>
+    </List.Item>
   );
 }
